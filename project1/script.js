ba80b3a32b84929ea07d73b241d3a95787117500
jingyu-f <79663022+jingyu-f@users.noreply.github.com>
2021-03-24 18:00:35 -0400

    pic

--- project1/script.js
+++ project1/script.js
@@ -1,4 +1,4 @@
-
+/*
 let xs =[]
 for (var i = 0; i <= 2000; i++){
   xs.push(i)
@@ -6,7 +6,7 @@ for (var i = 0; i <= 2000; i++){
 /*Statement 1 sets a variable before the loop starts (var i = 50).
 Statement 2 defines the condition for the loop to run (i must be less than 500).
 Statement 3 increases a value (i++) each time the code block in the loop has been executed.
-*/
+
 let t = 0
 function animate(){
   let points = xs.map(x => {
@@ -25,3 +25,4 @@ requestAnimationFrame(animate)
 }
 
 animate()
+*/

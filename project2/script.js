d52d5d488025913ad7363fc909482622b29bd31b
jingyu-f <79663022+jingyu-f@users.noreply.github.com>
2021-04-21 18:15:26 -0400

    test

--- project2/script.js
+++ project2/script.js
@@ -17,43 +17,6 @@ $('.axis-range').on('input', function(){
 
 });
 });
-let html = "";
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Providence&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
-
-  console.log(data);
-    let wind_providence = data["wind"]["speed"];
-
-    $('#wind_providence').text(wind_providence);
-
-
-   let hght = 0;
-   if (wind_providence !== 0){
-        hght = Math.round(wind_providence*100);
-   }
-
-   console.log(hght);
-
-   let setting = "'hght' " + hght;
-   $('.highlight').css('font-variation-settings', setting);
-   });
-//
-   $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Denver&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
-
-     console.log(data);
-       let wind_denver = data["wind"]["speed"];
-
-       $('#wind_denver').text(wind_denver);
-
-       let hght = 0;
-       if (wind_denver !== 0){
-            hght = Math.round(wind_denver*100);
-       }
-
-       console.log(hght);
-
-       let setting = "'hght' " + hght;
-       $('.highlight_1').css('font-variation-settings', setting);
-       });
 //mousemove
 $('#specimen').mousemove(function(event) {
 
@@ -69,3 +32,38 @@ $('#specimen').mousemove(function(event) {
   });
 
 });
+let html = "";
+$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Providence&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
+
+  console.log(data);
+    let wind_providence = data["wind"]["speed"];
+
+    $('#wind_providence').text(wind_providence);
+
+   let hght = 0;
+   if (wind_providence !== 0){
+        hght = Math.round(wind_providence*100);
+   }
+
+   console.log(hght);
+
+   let setting = "'hght' " + hght;
+   $('.highlight').css('font-variation-settings', setting);
+   });
+//
+$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Denver&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
+
+  console.log(data);
+    let wind_denver = data["wind"]["speed"];
+
+    $('#wind_denver').text(wind_denver);
+    let hght = 0;
+    if (wind_denver !== 0){
+         hght = Math.round(wind_denver*100);
+    }
+
+    console.log(hght);
+
+    let setting = "'hght' " + hght;
+    $('.highlight_1').css('font-variation-settings', setting);
+    });

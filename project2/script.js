c9081eb8b997c58d36c32a3695e0c37d3015f9fe
jingyu-f <79663022+jingyu-f@users.noreply.github.com>
2021-04-26 11:20:57 -0400

    try https

--- project2/script.js
+++ project2/script.js
@@ -34,7 +34,7 @@ $('#specimen').mousemove(function(event) {
 });
 
 let html = "";
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Providence&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
+$.getJSON('https://api.openweathermap.org/data/2.5/weather?q=Providence&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
 
   console.log(data);
     let wind_providence = data["wind"]["speed"];
@@ -43,7 +43,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Providence&appid=9f4
 
    let hght = 0;
    if (wind_providence !== 0){
-        hght = Math.round(wind_providence*100);
+        hght = Math.round(wind_providence*80);
    }
 
    console.log(hght);
@@ -52,7 +52,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Providence&appid=9f4
    $('.highlight').css('font-variation-settings', setting);
    });
 //
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Denver&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
+$.getJSON('https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
 
   console.log(data);
     let wind_denver = data["wind"]["speed"];
@@ -60,7 +60,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Denver&appid=9f4a66a
     $('#wind_denver').text(wind_denver);
     let hght = 0;
     if (wind_denver !== 0){
-         hght = Math.round(wind_denver*100);
+         hght = Math.round(wind_denver*80);
     }
 
     console.log(hght);
@@ -69,7 +69,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Denver&appid=9f4a66a
     $('.highlight_1').css('font-variation-settings', setting);
     });
 //
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
+$.getJSON('https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
 
   console.log(data);
     let wind_seattle = data["wind"]["speed"];
@@ -77,7 +77,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=9f4a66
     $('#wind_seattle').text(wind_seattle);
     let hght = 0;
     if (wind_seattle !== 0){
-         hght = Math.round(wind_seattle*90);
+         hght = Math.round(wind_seattle*80);
     }
 
     console.log(hght);
@@ -86,7 +86,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=9f4a66
     $('.highlight_2').css('font-variation-settings', setting);
     });
 //
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
+$.getJSON('https://api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
 
   console.log(data);
     let wind_sf = data["wind"]["speed"];
@@ -94,7 +94,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=
     $('#wind_sf').text(wind_sf);
     let hght = 0;
     if (wind_sf !== 0){
-         hght = Math.round(wind_sf*90);
+         hght = Math.round(wind_sf*80);
     }
 
     console.log(hght);
@@ -103,7 +103,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=
     $('.highlight_3').css('font-variation-settings', setting);
     });
 //
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
+$.getJSON('https://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
 
   console.log(data);
     let wind_dallas = data["wind"]["speed"];
@@ -111,7 +111,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=9f4a66a
     $('#wind_dallas').text(wind_dallas);
     let hght = 0;
     if (wind_dallas !== 0){
-         hght = Math.round(wind_dallas*90);
+         hght = Math.round(wind_dallas*80);
     }
 
     console.log(hght);
@@ -120,7 +120,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=9f4a66a
     $('.highlight_4').css('font-variation-settings', setting);
   });
 //
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
+$.getJSON('https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
 
   console.log(data);
     let wind_chicago = data["wind"]["speed"];
@@ -128,7 +128,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=9f4a66
     $('#wind_chicago').text(wind_chicago);
     let hght = 0;
     if (wind_chicago !== 0){
-         hght = Math.round(wind_chicago*90);
+         hght = Math.round(wind_chicago*80);
     }
 
     console.log(hght);
@@ -137,7 +137,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=9f4a66
     $('.highlight_5').css('font-variation-settings', setting);
   });
 //
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Detroit&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
+$.getJSON('https://api.openweathermap.org/data/2.5/weather?q=Detroit&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
 
   console.log(data);
     let wind_detroit = data["wind"]["speed"];
@@ -145,7 +145,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Detroit&appid=9f4a66
     $('#wind_detroit').text(wind_detroit);
     let hght = 0;
     if (wind_detroit !== 0){
-         hght = Math.round(wind_detroit*90);
+         hght = Math.round(wind_detroit*80);
     }
 
     console.log(hght);
@@ -153,8 +153,8 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Detroit&appid=9f4a66
     let setting = "'hght' " + hght;
     $('.highlight_6').css('font-variation-settings', setting);
   });
-  //
-  $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Washington&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
+//
+  $.getJSON('https://api.openweathermap.org/data/2.5/weather?q=Washington&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
 
     console.log(data);
       let wind_washington = data["wind"]["speed"];
@@ -162,7 +162,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Detroit&appid=9f4a66
       $('#wind_washington').text(wind_washington);
       let hght = 0;
       if (wind_washington !== 0){
-           hght = Math.round(wind_washington*90);
+           hght = Math.round(wind_washington*80);
       }
 
       console.log(hght);
@@ -170,8 +170,8 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Detroit&appid=9f4a66
       let setting = "'hght' " + hght;
       $('.highlight_7').css('font-variation-settings', setting);
     });
-    //
-    $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=New York&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
+//
+$.getJSON('https://api.openweathermap.org/data/2.5/weather?q=New York&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
 
       console.log(data);
         let wind_newyork = data["wind"]["speed"];
@@ -179,7 +179,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Detroit&appid=9f4a66
         $('#wind_newyork').text(wind_newyork);
         let hght = 0;
         if (wind_newyork !== 0){
-             hght = Math.round(wind_newyork*90);
+             hght = Math.round(wind_newyork*80);
         }
 
         console.log(hght);
@@ -188,7 +188,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Detroit&appid=9f4a66
         $('.highlight_8').css('font-variation-settings', setting);
       });
 //
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Boston&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
+$.getJSON('https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
 
   console.log(data);
     let wind_boston = data["wind"]["speed"];
@@ -196,7 +196,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Boston&appid=9f4a66a
     $('#wind_boston').text(wind_boston);
     let hght = 0;
     if (wind_boston !== 0){
-         hght = Math.round(wind_boston*90);
+         hght = Math.round(wind_boston*80);
     }
 
     console.log(hght);
@@ -205,7 +205,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Boston&appid=9f4a66a
     $('.highlight_9').css('font-variation-settings', setting);
   });
 //
-$.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Baltimore&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
+$.getJSON('https://api.openweathermap.org/data/2.5/weather?q=Baltimore&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
 
   console.log(data);
     let wind_baltimore = data["wind"]["speed"];
@@ -213,7 +213,7 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Baltimore&appid=9f4a
     $('#wind_baltimore').text(wind_baltimore);
     let hght = 0;
     if (wind_baltimore !== 0){
-         hght = Math.round(wind_baltimore*90);
+         hght = Math.round(wind_baltimore*80);
     }
 
     console.log(hght);
@@ -221,3 +221,20 @@ $.getJSON('http://api.openweathermap.org/data/2.5/weather?q=Baltimore&appid=9f4a
     let setting = "'hght' " + hght;
     $('.highlight_10').css('font-variation-settings', setting);
   });
+  //
+  $.getJSON('https://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=9f4a66a4052ffb8b92e0dd59a8d99657&units=imperial', function(data) {
+
+    console.log(data);
+      let wind_philadelphia = data["wind"]["speed"];
+
+      $('#wind_philadelphia').text(wind_philadelphia);
+      let hght = 0;
+      if (wind_philadelphia !== 0){
+           hght = Math.round(wind_philadelphia*80);
+      }
+
+      console.log(hght);
+
+      let setting = "'hght' " + hght;
+      $('.highlight_11').css('font-variation-settings', setting);
+    });

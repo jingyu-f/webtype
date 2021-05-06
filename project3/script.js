38e55016ff0a47675c687ef210b46c742d1bd2de
jingyu-f <79663022+jingyu-f@users.noreply.github.com>
2021-05-05 20:57:33 -0400

    dc

---
+++ project3/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+console.log('script loaded');
+
+const wghtmax = 500;
+const huemax = 360;
+
+
+  $('.axis-range').on('input', function(){
+
+    let value = parseInt($(this).val()); //get slider input value
+   //console.log(value);
+
+    const slidertype = $(this).data('type'); //get slider axis
+
+    console.log("--"+slidertype, value );
+
+    $('#shape').css("--"+slidertype, value);
+
+  });
+});

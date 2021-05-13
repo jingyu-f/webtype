3b448094a0aceca0b5bb16b0a0a8302a751e7aef
jingyu-f <79663022+jingyu-f@users.noreply.github.com>
2021-05-13 04:39:06 -0400

    0

--- project3/script.js
+++ project3/script.js
@@ -3,21 +3,47 @@ $(document).ready(function(){
 console.log('script loaded');
 
 const wghtmax = 500;
-const huemax = 360;
+const lummax = 100;
 
-
+let value;
 
   $('.axis-range').on('input', function(){
 
-    let value = parseInt($(this).val()); //get slider input value
-   //console.log(value);
+    value = parseInt($(this).val()); //get slider input value
+    console.log(value);
 
     const slidertype = $(this).data('type'); //get slider axis
 
-    console.log("--"+slidertype, value );
+      console.log("--"+slidertype, value);
 
-    $('#shape').css("--"+slidertype, value);
-
-
+     $('#shape').css("--"+slidertype, value);
   });
+
+  $('form').submit(function(e){
+	    console.log('submit');
+
+	    let plain = $('#plain').val();
+	    $('#word').text(plain);
+
+      //let shape = $('#shape').val();
+      //$('#newshape').text(shape);
+      //let css = $('#shape').css();
+      //$('#newshape').css(css);
+
+      $('form').hide();
+      $('#poster').show();
+      // $('#newshape').append(cellname);
+      //
+      //
+	   	// let chocolate = $('#options').val();
+	    // $('#chocolate-choice').text(chocolate);
+      //
+	    // $('form').hide(); //hide form upon submission
+      //
+	    // $('#output').show(); //show output upon submission
+
+	    return false;
+	});
+
+
 });
